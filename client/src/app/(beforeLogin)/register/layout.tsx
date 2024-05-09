7d613050f346c97584fba9322fbe2989986984d0
raihanonad <raihansetiawan3296@gmail.com>
2024-05-09 20:33:08 +0700

    fix register layout

--- client/src/app/(beforeLogin)/register/layout.tsx
+++ client/src/app/(beforeLogin)/register/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
+import React from "react";
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
     <>
       {children}
