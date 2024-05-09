47e48fc2e96de7d5ce9ef9553b7a2b7040bab2e2
raihanonad <raihansetiawan3296@gmail.com>
2024-05-09 19:50:30 +0700

    feat add product slug route

---
+++ client/src/app/api/products/[slug]/route.ts
@@ -0,0 +1,13 @@
+import ProductModel from "@/db/models/product";
+
+export async function GET(request: Request,
+    { params }: { params: { slug: string } }
+    ) {
+    
+    const product = await ProductModel.getProductBySlug(params.slug);
+
+    return Response.json({
+        data: product
+    }, {status: 200});
+}
+    
